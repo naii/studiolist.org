59c9b902fbcf71dfecf87e5cc4df24e31da25f26
Alexander Kluge <spam@naii.de>
2019-04-13 16:54:06 -0700

    Added new mission statement

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer>
+	<p><em>studiolist.org is <a href="https://2017.ind.ie/ethical-design/">ethical by design</a>, and it’s <a href="https://github.com/naii/studiolist.org">open-source</a> because it stands for trust and transparency. I want to empower voiceover artists to <strong>work in top-rated studios while traveling to awesome cities</strong> that nourish their creative output and make them connect to the world. You can still have your home-recording studio but what is more exciting than to actually visit the cities you always wanted to go to and still deliver outstanding voiceover performances?</em></p>
 	<hr class="below-the-fold call-to-action centered">
 	<p class="small centered">by <a href="https://naii.io">naii.io</a> — <a rel="me" href="https://mastodon.naii.io/@naii">let’s talk on Mastodon</a> or <a href="https://t.me/alexanderkluge">send a telegram</a></p>
 </footer>
