68ecac13cd1a94c6e08ee475e26a929123d88e5c
Alexander Kluge <spam@naii.de>
2019-10-19 01:11:31 +0200

    Removed the rel="me" to have the site not being green highlighted (as mine) in my Mastodon profile

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 	
 	<hr class="below-the-fold call-to-action centered">
 	
-	<p class="small centered">by <a href="https://naii.io">naii.io</a> — <a rel="me" href="https://mastodon.naii.io/@naii">let’s talk on Mastodon</a> or <a href="https://t.me/alexanderkluge">send a telegram</a></p>
+	<p class="small centered">by <a href="https://naii.io">naii.io</a> — <a href="https://mastodon.naii.io/@naii">let’s talk on Mastodon</a> or <a href="https://t.me/alexanderkluge">send a telegram</a></p>
 
 	<hr class="below-the-fold call-to-action centered">
 
