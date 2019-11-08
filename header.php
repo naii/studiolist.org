2e56c1c35cd8f56362c44d5f6707f0d59fc225c3
Alexander Kluge <spam@naii.de>
2019-11-08 21:25:58 +0100

    Added Matomo tracking

--- header.php
+++ header.php
@@ -10,6 +10,21 @@
 	<meta name="description" content="Recording studio search engine for traveling YouTubers, podcasters, voiceover artists and audiobook narrators">
 	<link rel="stylesheet" href="/style.css">
 	<link rel="stylesheet" href="<?php echo $other_stylesheet; ?>">
+	<!-- Matomo -->
+	<script type="text/javascript">
+	  var _paq = window._paq || [];
+	  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	  _paq.push(['trackPageView']);
+	  _paq.push(['enableLinkTracking']);
+	  (function() {
+	    var u="//studiolist.org/m/";
+	    _paq.push(['setTrackerUrl', u+'matomo.php']);
+	    _paq.push(['setSiteId', '1']);
+	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+	  })();
+	</script>
+	<!-- End Matomo Code -->
 </head>
 <body>
 		<div id="main">
